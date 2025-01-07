92fc3cdb4c66da05b5354d4b7721f3418b66d967
naaam-h-siddhu <sudhakarsingh2004@gmail.com>
2025-01-07 12:28:08 +0530

    final calculator

---
+++ calculator.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class calculator{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int a = sc.nextInt();
+        System.out.println("Enter the second number");
+        int b = sc.nextInt();
+        System.out.println("Enter the operator");
+        char op = sc.next().charAt(0);
+        switch (op){
+            case '+':
+                System.out.println(a+b);
+                break;
+            case '-':
+                System.out.println(a-b);
+                break;
+            case '*':
+                System.out.println(a*b);
+                break;
+            case '/':
+                System.out.println(a/b);
+                break;
+            default:
+                System.out.println("Please enter the valid character");
+        }
+    }
+}
